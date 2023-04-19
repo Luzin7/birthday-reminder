c084fc2b2c8326e9b735300ffd23a379672e9791
Luzin7 <lvictor.dutra@gmail.com>
2023-04-19 19:09:25 -0300

    feat(getUsersBirthda.ts): cria dados falsos

---
+++ src/data/getUsersBirthday.ts
@@ -0,0 +1,41 @@
+interface ObjectProp {
+	id: number;
+	name: string;
+	age: number;
+	image: string;
+}
+
+const usersBirthdayList: ObjectProp[] = [
+	{
+		id: 1,
+		name: 'Luan Victor',
+		age: 21,
+		image: 'https://avatars.githubusercontent.com/u/76531786?v=4',
+	},
+	{
+		id: 2,
+		name: 'Rafael Lima',
+		age: 20,
+		image: 'https://avatars.githubusercontent.com/u/48070538?v=4',
+	},
+	{
+		id: 3,
+		name: 'Lucas Mafra',
+		age: 19,
+		image: 'https://avatars.githubusercontent.com/u/39749683?v=4',
+	},
+	{
+		id: 4,
+		name: 'Moésio Filho',
+		age: 20,
+		image: 'https://avatars.githubusercontent.com/u/38077594?v=4',
+	},
+	{
+		id: 5,
+		name: 'Anderson Bernardo',
+		age: 21,
+		image: 'https://avatars.githubusercontent.com/u/37579786?v=4',
+	},
+];
+
+export default usersBirthdayList;
